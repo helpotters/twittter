61ff90c7158d0769cf23320994984e17fa717d79
Paul Lemus <paullemus@protonmail.com>
2022-03-21 01:27:44 -0700

    feat: added user accounts
    Users can now sign up, log in, and post tweets.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_21_055823) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_21_075312) do
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
